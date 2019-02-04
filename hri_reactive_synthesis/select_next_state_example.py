4d6da0f16507b2b495b149cb46c580565b0c9ed6
Rachel Schlossman <rachel.schlossman@utexas.edu>
2019-02-04 17:59:17 -0600

    testing check_dictionary_comparisons in select_next_state_example.py

--- hri_reactive_synthesis/select_next_state_example.py
+++ hri_reactive_synthesis/select_next_state_example.py
@@ -20,19 +20,21 @@ def check_dictionary_comparisons(nodes_dict, transitions_dict):
 
 	check = nodes_dict[key_random]
 
-	print "random key", key_random
+	check = {'workload':15, 'obstacle2': 0, 'obstacle3': 0, 'workload_stays_constant': 1, 'complete_work_with_robot': 0, 'arriving_at_0': 0, 'r_state': 2, 'workload_add': 0, 'complete_work_at_workstation': 1, 'complete_dropoff_tries': 0, 'next_state_is_workstation': 0, 'complete_dropoff_success': 0, 'wait': 0}
+	# print "random key", key_random
 
 	# Testing if we can compare dictionaries
 	for key, node in nodes_dict.items():
-		print nodes_dict[key]
-		exit()
+
 		if nodes_dict[key] == check:
 
 			print "selected key", key 
 			transition_options = transitions_dict[key]
 			print "transition_options", transition_options
-			print "next node", (random.choice(transition_options))
+			next_node = random.choice(transition_options)
+			print "next node", (next_node)
 			
+			print nodes_dict[key]['r_state']
 
 	return
 
@@ -65,7 +67,7 @@ def main():
 	    transitions_dict = json.load(f)
 
 	check_dictionary_comparisons(nodes_dict, transitions_dict)
-	test_transitions(nodes_dict, transitions_dict)
+	# test_transitions(nodes_dict, transitions_dict)
 
 
 
